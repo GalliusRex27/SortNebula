e49fc9b7bedb46df873a983908676a1aa6ae5402
Bala Aditya <49568060+GalliusRex27@users.noreply.github.com>
2019-05-20 13:06:01 +0530

    Update mergesort.cpp

--- mergesort.cpp
+++ mergesort.cpp
@@ -2,49 +2,49 @@
 using namespace std;
 void merge(int arr[],int brr[], int l, int m, int r) 
 {  
-    int o=m-l+1; 
-    int p=r-m; 
+    int o = m - l + 1; 
+    int p = r - m; 
     int A[o],B[p],C[o],D[p];
-    for (int i=0;i<o;i++) {
-        A[i]=arr[l+i]; 
-        C[i]=brr[l+i];}
-    for (int j=0;j<p;j++){
-        B[j]=arr[m+1+j];
-        D[j]=brr[m+1+j];}
-    int i=0,j=0,k=l;
-    while (i<o&&j<p) 
+    for (int i = 0;i<o;i++) {
+        A[i] = arr[l+i]; 
+        C[i] = brr[l+i];}
+    for (int j = 0;j<p;j++){
+        B[j] = arr[m+1+j];
+        D[j] = brr[m+1+j];}
+    int i = 0,j = 0,k = l;
+    while (i<o && j<p) 
 {   if(A[i]<=B[j]) 
     { 
-        arr[k]=A[i];
-        brr[k]=C[i];
-	    i++; 
+        arr[k] = A[i];
+        brr[k] = C[i];
+	i++; 
     } 
-        else
+    else
     { 
-	    arr[k]=B[j];
-		brr[k]=D[j]; 
-        j++; 
+       arr[k] = B[j];
+       brr[k]=D[j]; 
+       j++; 
     } 
     k++; 
 } 
-    while (i<o) 
+    while (i < o) 
 { 
         arr[k]=A[i];
-		brr[k]=C[i]; 
+	brr[k]=C[i]; 
         i++; 
         k++; 
 } 
     while (j<p) 
 { 
         arr[k]=B[j];
-		brr[k]=D[j]; 
+	brr[k]=D[j]; 
         j++; 
         k++; 
 } 
 } 
 void mergeSort(int arr[],int brr[],int l,int r) 
 { 
-    if (l<r) {
+    if (l < r) {
     int e =l+(r-l)/2; 
     mergeSort(arr,brr,l,e); 
     mergeSort(arr,brr,e+1,r); 
@@ -56,17 +56,17 @@ int main(){
 	cout<<"Enter the pairs";
 	int A[20],B[20];
 	for(int i=0;i<n;i++){
-		cin>>A[i];
-		cin>>B[i];
+	    cin>>A[i];
+            cin>>B[i];
 	}
 	mergeSort(A,B,0,n-1);
-    vector< pair<int,int> >vector1;
-	for(int i=0;i<n;i++){
-		vector1.push_back(make_pair(A[i],B[i]));
+        vector< pair<int,int> >vector1;
+	for(int i = 0;i<n;i++){
+	    vector1.push_back(make_pair(A[i],B[i]));
 	}
 	cout<<"The sorted pairs, by mergesort, are "<<endl;
-	for(int i=0;i<n;i++){
-		cout<<vector1[i].first<<" "<<vector1[i].second<<endl;
+	for(int i = 0;i<n;i++){
+	    cout<<vector1[i].first<<" "<<vector1[i].second<<endl;
 	}
 	return 0;
 }
